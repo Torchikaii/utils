7008ce125e1daafee3c1edd1e6e7881f3b469056
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2026-01-30 23:10:11 +0200

    update docker.sh script with actual docker installation

--- ubuntu-utility/docker.sh
+++ ubuntu-utility/docker.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
-echo "Hello World"
+
+# Automatic docker installation script, based on https://docs.docker.com/engine/install/ubuntu/
+
+
+# uninstall conflicting packages
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+
+# Set up docker's apt repository
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+# install the docker packages
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# The Docker service starts automatically after installation. To verify that Docker is running, use
+
+sudo systemctl status docker
+
+# Some systems may have this behavior disabled and will require a manual start
+sudo systemctl start docker
+
+# Verify that the installation is successful by running the hello-world image
+sudo docker run hello-world
 
