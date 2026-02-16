2999c0729446d336e938be75b503798f340fa630
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2026-02-16 23:49:09 +0200

    update README.md

--- ubuntu-utility/ssh.sh
+++ ubuntu-utility/ssh.sh
@@ -4,6 +4,8 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
+echo "ssh.sh running..."
+
 sudo apt update -y
 sudo apt install -y openssh-server > /dev/null 2>&1
 sudo systemctl start ssh > /dev/null 2>&1
