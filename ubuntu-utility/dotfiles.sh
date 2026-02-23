3addd50597a194a884d71ae4999c0ebae87a96ea
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2026-02-23 14:09:21 +0200

    add symlink for .bashrc

--- ubuntu-utility/dotfiles.sh
+++ ubuntu-utility/dotfiles.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 
 echo "dotfiles.sh running..."
 
+
+#------------------------------
+#  Alacritty
+
 echo "setting up dotfiles for allacrity"
-# alacritty
+
 ALACRITTY_CONFIG_DIR="$HOME/.config/alacritty"
 ALACRITTY_CONFIG="$ALACRITTY_CONFIG_DIR/alacritty.toml"
 
@@ -13,5 +17,17 @@ mkdir -p "$ALACRITTY_CONFIG_DIR"
 
 rm -f "$ALACRITTY_CONFIG"
 ln -s "$HOME/repos/utils/ubuntu-utility/dotfiles/alacritty/alacritty.toml" "$ALACRITTY_CONFIG"
+
+
+#-------------------------------
+# Bash
+
+echo "setting up dotfiles for bash"
+
+BASH_CONFIG="$HOME/.bashrc"
+
+rm -f "$BASH_CONFIG"
+ln -s "$HOME/repos/utils/ubuntu-utility/dotfiles/bash/.bashrc" "$BASH_CONFIG"
+
 
 echo "dotfiles.sh finished"
