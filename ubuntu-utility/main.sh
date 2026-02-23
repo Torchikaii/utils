ed9d9acad61f5db22d2be8630b6284b4e2e9a3d6
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2026-02-23 14:49:36 +0200

    move files to other directories

--- ubuntu-utility/main.sh
+++ ubuntu-utility/main.sh
@@ -2,15 +2,19 @@
 
 set -e
 
+# apps
+./ubuntu-utility/apps/docker.sh
+./ubuntu-utility/apps/terraform.sh
+./ubuntu-utility/apps/opencode.sh
+./ubuntu-utility/apps/brave.sh
+./ubuntu-utility/apps/alacritty.sh
+./ubuntu-utility/apps/apt-apps.sh
+
+# services
+./ubuntu-utility/services/ssh.sh
+
+# system configuration
+./ubuntu-utility/dotfiles.sh
 ./ubuntu-utility/keyboard.sh
 
-./ubuntu-utility/docker.sh
-./ubuntu-utility/ssh.sh
-./ubuntu-utility/terraform.sh
-./ubuntu-utility/opencode.sh
-./ubuntu-utility/brave.sh
-./ubuntu-utility/alacritty.sh
-./ubuntu-utility/apt-apps.sh
 
-
-./ubuntu-utility/dotfiles.sh
