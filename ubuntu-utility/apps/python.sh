cf22a95e6a06438bf564cb31a29b630d0d9c0fd0
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2026-03-04 22:26:32 +0200

    python.sh is now safe to re-run

--- ubuntu-utility/apps/python.sh
+++ ubuntu-utility/apps/python.sh
@@ -4,11 +4,14 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
+# temporarely export in, as .bashrc is loaded later
 export PATH="$HOME/.pyenv/bin:$PATH"  # pyenv
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"   
 
 
-pyenv install 3.12.12
+if ! pyenv versions | grep -q "3.12.12"; then
+    pyenv install 3.12.12
+fi
 pyenv global 3.12.12 
